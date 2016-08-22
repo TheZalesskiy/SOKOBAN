b0fc2f6a4bd48e2e6e9a647295189ea0bf1d533c
Evgeniy Zalesskiy <evgeniy.zalesskiy@gmail.com>
2016-08-22 12:38:08 +0300

    Update EventListener.java

--- src/main/java/controller/EventListener.java
+++ src/main/java/controller/EventListener.java
@@ -9,8 +9,8 @@ import model.Direction;
  * Created by TheZalesskie
  */
 public interface EventListener {
-    void move(Direction direction); // передвинуть объект в определенном направлении.
-    void restart();                 // начать заново текущий уровень.
-    void startNextLevel();          // начать следующий уровень.
-    void levelCompleted(int level); // уровень с номером level завершён.
+    void move(Direction direction); // move an object in a certain direction.
+    void restart();                 // start again the current level.
+    void startNextLevel();          // start the next level.
+    void levelCompleted(int level); // level with the finished level number.
 }
